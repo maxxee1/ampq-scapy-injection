104e4e3146a603aac0b0b3e3f38f77d9aac8ca66
Maximiliano <130422048+maxxee1@users.noreply.github.com>
2025-07-04 20:18:25 -0400

    Update Insaneador.py

--- RabbitMQ/el_enviador/Insaneador.py
+++ RabbitMQ/el_enviador/Insaneador.py
@@ -1,1 +1,31 @@
+import pika
+import time
 
+print("🐇 Insaneador despertando...")
+
+# Intentos infinitos
+while True:
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host='rabbit',
+                port=5672,
+                credentials=pika.PlainCredentials('MAXI', 'MAXI')
+            )
+        )
+        print("🐇 Conejo encontrado, enviando 💌")
+        break
+    except pika.exceptions.AMQPConnectionError:
+        print("🐇 Conejo dormido, retry en 5s 🫠")
+        time.sleep(5)
+
+channel = connection.channel()
+channel.queue_declare(queue='insane_queue')
+
+channel.basic_publish(exchange='',
+                      routing_key='insane_queue',
+                      body='Holi insaneado 🫶✨🐇')
+
+print(" [x] Enviado: Holi insaneado")
+
+connection.close()
